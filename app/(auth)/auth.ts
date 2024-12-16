0c1a35e44304864773c612f35f189224d0b9079f
aa <Nahida-aa@outlook.com>
2024-12-16 18:51:47 +0800

    add: cookie

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -1,35 +1,35 @@
-// // "use server";
-// import { headers as nextHeaders } from "next/headers"
-// import { cookies } from 'next/headers'
-// import { AuthAction } from "./core/types";
+// "use server";
+import { headers as nextHeaders } from "next/headers"
+import { cookies } from 'next/headers'
+import { AuthAction } from "./core/types";
 // import { Auth, AuthConfig } from "./core";
 // import { NextAuthConfig } from "./core/lib";
-// import { redirect } from "next/navigation";
-// import { raw, skipCSRFCheck } from "./core/lib/symbols";
+import { redirect } from "next/navigation";
+import { raw, skipCSRFCheck } from "./core/lib/symbols";
 
-// export async function auth(){
-//   const cookieStore = cookies();
-//   const token = cookieStore.get('access_token');
+export async function auth(){
+  const cookieStore = cookies();
+  const token = cookieStore.get('access_token');
   
-//   if (!token) {
-//     return null;
-//   }
-//   // 解密 token
-//   const user = JSON.parse(atob(token.value.split('.')[1]));
-//   return { user };
-// }
+  if (!token) {
+    return null;
+  }
+  // 解密 token
+  const user = JSON.parse(atob(token.value.split('.')[1]));
+  return { user };
+}
 
-// type SignInParams = [(string & {}) | undefined, 
-//   options?: FormData | ({
-//     redirectTo?: string;
-//     redirect?: boolean | undefined;
-// } & Record<string, any>) | undefined,
-//   authorizationParams?:
-//   | string[][]
-//   | Record<string, string>
-//   | string
-//   | URLSearchParams
-// ]
+type SignInParams = [(string & {}) | undefined, 
+  options?: FormData | ({
+    redirectTo?: string;
+    redirect?: boolean | undefined;
+} & Record<string, any>) | undefined,
+  authorizationParams?:
+  | string[][]
+  | Record<string, string>
+  | string
+  | URLSearchParams
+]
 // export async function signIn(
 //   provider: SignInParams[0],
 //   options: SignInParams[1] = {},
@@ -96,41 +96,41 @@
 //   // }
 // }
 
-// type SignOutParams = [options?: { 
-//   redirectTo?: string; 
-//   // The relative path to redirect to after signing out. By default, the user is redirected to the current page.
-//   // 登出后重定向的相对路径。默认情况下，用户将被重定向到当前页面。
-//   redirect?: boolean|undefined 
-//   // If set to `false`, the `signOut` method will return the URL to redirect to instead of redirecting automatically. 
-//   // 如果设置为 `false`，`signOut` 方法将返回要重定向到的 URL，而不是自动重定向。
-//   }|undefined
-// ];
-// // export const signOut = async <R extends boolean = true>(options: SignOutParams[0] = {
-// // }): Promise<R extends false ? any : never> => {
-// //   const headers = new Headers(await nextHeaders())
+type SignOutParams = [options?: { 
+  redirectTo?: string; 
+  // The relative path to redirect to after signing out. By default, the user is redirected to the current page.
+  // 登出后重定向的相对路径。默认情况下，用户将被重定向到当前页面。
+  redirect?: boolean|undefined 
+  // If set to `false`, the `signOut` method will return the URL to redirect to instead of redirecting automatically. 
+  // 如果设置为 `false`，`signOut` 方法将返回要重定向到的 URL，而不是自动重定向。
+  }|undefined
+];
+// export const signOut = async <R extends boolean = true>(options: SignOutParams[0] = {
+// }): Promise<R extends false ? any : never> => {
+//   const headers = new Headers(await nextHeaders())
 
-// //   headers.set("Content-Type", "application/x-www-form-urlencoded")
-// //   const url = createActionURL(
-// //     "signout",
-// //     // @ts-expect-error `x-forwarded-proto` is not nullable, next.js sets it by default
-// //     headers.get("x-forwarded-proto"),
-// //     headers,
-// //     // process.env,
-// //     // config
-// //   )
-// //   const callbackUrl = options?.redirectTo ?? headers.get("Referer") ?? "/"
-// //   const body = new URLSearchParams({ callbackUrl })
-// //   const req = new Request(url, { method: "POST", headers, body })
-// //   const res = await Auth(req, { 
-// //     // ...config, 
-// //     raw, skipCSRFCheck })
+//   headers.set("Content-Type", "application/x-www-form-urlencoded")
+//   const url = createActionURL(
+//     "signout",
+//     // @ts-expect-error `x-forwarded-proto` is not nullable, next.js sets it by default
+//     headers.get("x-forwarded-proto"),
+//     headers,
+//     // process.env,
+//     // config
+//   )
+//   const callbackUrl = options?.redirectTo ?? headers.get("Referer") ?? "/"
+//   const body = new URLSearchParams({ callbackUrl })
+//   const req = new Request(url, { method: "POST", headers, body })
+//   const res = await Auth(req, { 
+//     // ...config, 
+//     raw, skipCSRFCheck })
 
-// //   const cookieStore = cookies();
-// //   for (const c of res?.cookies ?? []) cookieStore.set(c.name, c.value, c.options)
+//   const cookieStore = cookies();
+//   for (const c of res?.cookies ?? []) cookieStore.set(c.name, c.value, c.options)
 
-// //   if (options?.redirect ?? true) return redirect(res.redirect!)
-// //   return res as any
-// // }
+//   if (options?.redirect ?? true) return redirect(res.redirect!)
+//   return res as any
+// }
 
 
 // export function createActionURL(
