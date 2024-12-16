9be8d6b93d91244eda983897541bd3347604e601
aa <Nahida-aa@outlook.com>
2024-12-16 18:33:04 +0800

    add: cookie

--- app/(auth)/core/lib/utils/logger.ts
+++ app/(auth)/core/lib/utils/logger.ts
@@ -6,12 +6,6 @@ export type WarningCode = // 4
   | "env-url-basepath-mismatch"
 
 
-/**
- * Override any of the methods, and the rest will use the default logger.
- *
- * [Documentation](https://authjs.dev/reference/core#authconfig#logger)
- */
-// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export interface LoggerInstance extends Record<string, Function> { // 17
   warn: (code: WarningCode) => void
   error: (error: Error) => void
